a4c8685c6f55b1e849fe346fb9506bf00a39c4bb
finote21 <finotepawloss29@gmail.com>
2024-08-23 20:14:20 -0700

    row has been added

--- netflix-clone/src/pages/Home/Home.js
+++ netflix-clone/src/pages/Home/Home.js
@@ -2,15 +2,28 @@ import React from "react";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import Banner from "../../components/Banner/Banner";
+import Row from "../../components/Rows/Row";
+import requests from "../../Utils/requests";
 
-const Home = () => {
+function Home() {
   return (
-    <div>
+    <>
       <Header />
       <Banner />
+      <Row
+        title="Netflix Orginals"
+        fetchurl={requests.fetchNetflexOrginals}
+        isLargeRow={true}
+      />
+      <Row title="Trendings" fetchurl={requests.fetchTrending} />
+      <Row title="Top Rated " fetchurl={requests.fetchTopRated} />
+      <Row title="Action Movies " fetchurl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies " fetchurl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies " fetchurl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies " fetchurl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries " fetchurl={requests.fetchDocumentaries} />
       <Footer />
-    </div>
+    </>
   );
-};
-
+}
 export default Home;
